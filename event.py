61b4d712aa49cd3826e33fad8fa47eb7a8ff69a0
joe <joe.broder@berkeley.edu>
2023-04-12 20:21:20 -0400

    fixed get_state and added support for multiple private ids

--- event.py
+++ event.py
@@ -6,12 +6,12 @@ EventType = Enum("EventType", ["WIN", "LOSE", "MOVE", "ACCUSE", "SUGGEST", "STAR
 class Event:
     event_count = 0
 
-    def __init__(self, event_type, public_response, private_response, private_ID):
+    def __init__(self, event_type, public_response, private_response, private_IDs):
         self.event_ID = Event.event_count
         self.event_type = event_type
         self.public_response = public_response
         self.private_response = private_response
-        self.private_ID = private_ID
+        self.private_IDs = private_IDs
         Event.event_count += 1
 
     def get_state(self):
@@ -20,5 +20,5 @@ class Event:
             "event_type": self.event_type,
             "public_response": self.public_response,
             "private_response": self.private_response,
-            "private_ID": self.private_ID,
+            "private_IDs": list(self.private_IDs),
         }
--- test_event.py
+++ test_event.py
@@ -3,8 +3,10 @@ from event import *
 
 def test_event_id():
     def helper():
-        local_event = Event(EventType.WIN, "public", "private", "some id")
+        local_event = Event(
+            EventType.WIN, "public", "private", {"some id", "another id"}
+        )
 
     helper()
-    e = Event(EventType.WIN, "public", "private", "some id")
+    e = Event(EventType.WIN, "public", "private", {"some id", "another id"})
     assert e.event_ID == 1
