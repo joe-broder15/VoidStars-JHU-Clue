9b34b7e63c80cb0c72d162214a3f4675d50d0898
joe <joe.broder@berkeley.edu>
2023-04-27 23:37:11 -0400

    about to do proper controls

--- clueless-client/src/components/EventLog.js
+++ clueless-client/src/components/EventLog.js
@@ -3,14 +3,18 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
+import Container from 'react-bootstrap/esm/Container';
 
 // component exists to hold control and display subcomponents
-function EventLog({events}) {
+function EventLog({ events }) {
     // console.log(events[0]);
     return (
-        <Card className='eventLogCard'>
-            {events.map((e) => <p>{e.response}</p>)}
-        </Card>
+        <Container className='eventLogCard'>
+            {/* <Card > */}
+                {events.map((e) => <p>{e.response}</p>)}
+            {/* </Card> */}
+        </Container>
+
     );
 };
 
